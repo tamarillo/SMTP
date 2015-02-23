d0a597d8b61c5cbc1d492d0572a0f4c92450d2b9
dengohphp <developer@dengophp.de>
2015-02-23 09:58:31 +0100

    Composer init

---
+++ src/Connection.php
@@ -0,0 +1,12 @@
+<?php namespace Smtp;
+
+/**
+ * Class SmtpConnection
+ *
+ * @author tmaxham
+ * @generated 23.02.15
+ * @version 23.02.15
+ */
+class Connection {
+
+} 
---
+++ src/Entity.php
@@ -0,0 +1,12 @@
+<?php namespace Smtp;
+
+/**
+ * Class Entity
+ *
+ * @author tmaxham
+ * @generated 23.02.15
+ * @version 23.02.15
+ */
+class Entity {
+
+} 
